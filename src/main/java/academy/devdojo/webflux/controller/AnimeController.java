e248c827ed5af6687f2f1c9fd53e1194b6483375
Luiz Souza <luiz@pop-os.localdomain>
2023-09-04 16:19:05 -0300

    first commit

---
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -0,0 +1,43 @@
+package academy.devdojo.webflux.controller;
+
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.service.AnimeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("animes")
+@Slf4j
+public class AnimeController {
+    private final AnimeService animeService;
+
+    @GetMapping
+    public Flux<Anime> listAll() {
+        return animeService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Mono<Anime> findById(@PathVariable long id) {
+        return animeService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Anime> save(@Valid @RequestBody Anime anime) {
+        return animeService.save(anime);
+    }
+
+    @PutMapping(path = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> update(@PathVariable long id, @Valid @RequestBody Anime anime) {
+        return animeService.update(anime.withId(id));
+    }
+
+}
---
+++ src/main/java/academy/devdojo/webflux/service/AnimeService.java
@@ -0,0 +1,40 @@
+package academy.devdojo.webflux.service;
+
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.repository.AnimeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class AnimeService {
+
+    private final AnimeRepository animeRepository;
+
+    public Flux<Anime> findAll() {
+        return animeRepository.findAll();
+    }
+
+    public Mono<Anime> findById(Long id) {
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException());
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+    }
+
+    public Mono<Anime> save(Anime anime) {
+        return  animeRepository.save(anime);
+    }
+
+    public Mono<Void> update(Anime anime) {
+        return findById(anime.getId())
+                .flatMap(validAnime -> animeRepository.save(anime))
+                .then();
+    }
+}
